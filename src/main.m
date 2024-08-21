3149633b8cb8d2694299a44d0f51ef4b20389bf8
TheCarmelGuy <komelm13@gmail.com>
2024-08-21 12:01:52 -0400

    added stuff

--- src/main.m
+++ src/main.m
@@ -31,14 +31,14 @@ error_C = my_psnr(C,cast_double(inputImage))
 f1 = figure;
 subplot(1,3,1)
 imshow(cast_int(a));
-title(['Reconstrction A; PSNR = ',num2str(error_a)]);
+title(['Reconstruction A; PSNR = ',num2str(error_a)]);
 subplot(1,3,2)
 imshow(cast_int(B));
-title(['Reconstrction B; PSNR = ',num2str(error_B)]);
+title(['Reconstruction B; PSNR = ',num2str(error_B)]);
 
 subplot(1,3,3)
 imshow(cast_int(C));
-title(['Reconstrction C; PSNR = ',num2str(error_C)]);
+title(['Reconstruction C; PSNR = ',num2str(error_C)]);
 
 %
 
@@ -59,9 +59,9 @@ e_9= compute_entropy(patch_9);
 e_10= compute_entropy(patch_10); 
 
 f_en = figure
-bar([e_0 e_1 e_2 e_3 e_4 e_5 e_6 e_7 e_8 e_9 e_10])
-title('Entropy');
-
+names = categorical(["original image" "subband_1" "subband_2" "subband_3" "subband_4" "subband_5" "subband_6" "subband_7" "subband_8" "subband_9" "subband_{10}"]);
+bar(names,[e_0 e_1 e_2 e_3 e_4 e_5 e_6 e_7 e_8 e_9 e_10])
+title('Sub-band Entropy')
 
 
 
@@ -90,8 +90,8 @@ title(['Reconstructed LLLL; PSNR = ',num2str(e1)]);
 
 %Level 2
 s_2 = 1.0;
-t_2 = 0.2;
-mask_2 = find_zero_crossing(recon_llll, s_1, t_1);
+t_2 = 0.1;
+mask_2 = find_zero_crossing(recon_llll, s_2, t_2);
 patch_5_down = copy_from_mask(mask_2,patch_5);
 patch_6_down = copy_from_mask(mask_2,patch_6);
 patch_7_down = copy_from_mask(mask_2,patch_7);
@@ -111,7 +111,7 @@ title(['Reconstructed LL; PSNR = ',num2str(e2)]);
 
 %Level 3
 s_3 = 1.0;
-t_3 = 0.2;
+t_3 = 0.1;
 mask_3 = find_zero_crossing(recon_llll, s_3, t_3);
 patch_8_down = copy_from_mask(mask_2,patch_8);
 patch_9_down = copy_from_mask(mask_2,patch_9);
